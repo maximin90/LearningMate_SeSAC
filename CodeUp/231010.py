1c7292caaaa214ca5145931d137d65062ffb0a4b
maxmin90 <91pp90@gmail.com>
2023-10-10 22:54:39 +0900

    2일차 코테 문제풀이 완료

---
+++ CodeUp/231010.py
@@ -0,0 +1,16 @@
+# 6006 
+print('\"!@#$%^&*()\'')
+
+# 6007
+print("\"C:\Download\\'hello\'.py\"")
+
+# 6008
+print('print("Hello\\nWorld")')
+
+# 6009
+v = input()
+print(v)
+
+# 6010
+num = int(input())
+print(num)
