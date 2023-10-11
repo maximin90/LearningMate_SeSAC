6305d905f3993e604ca093aa4425a8102af4c802
maxmin90 <91pp90@gmail.com>
2023-10-12 01:10:08 +0900

    3일차 코테 문제풀이 완료

---
+++ CodeUp/231011.py
@@ -0,0 +1,27 @@
+# 6011
+f = float(input())
+print(f)
+
+# 6012
+a = int(input())
+b = int(input())
+print(a)
+print(b)
+
+# 6013
+cht = input()
+cht2 = input()
+print(cht2)
+print(cht)
+
+# 6014
+f = float(input())
+print(f )
+print(f )
+print(f )
+
+# 6015
+a, b = map(int, input().split()) 
+# int 함수는 리스트를 정수형으로 바꾸어 줄 수 없음 
+print(a)
+print(b)
