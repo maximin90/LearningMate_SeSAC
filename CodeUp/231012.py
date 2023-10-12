4cdbedb0ee4ed8e627dc57c2b3a6dcec5aaadf2e
maxmin90 <91pp90@gmail.com>
2023-10-12 11:01:51 +0900

    4일차 코테 문제풀이 완료

---
+++ CodeUp/231012.py
@@ -0,0 +1,19 @@
+# 6016
+c1, c2 = map(str, input().split())
+print(c2, c1)
+
+# 6017
+s = input()
+print(s, s, s)
+
+# 6018
+a, b = input().split(':')
+print(a, b, sep=':')
+
+# 6019
+a, b, c = input().split('.')
+print(c, b, a, sep = '-')
+
+# 6020
+a, b = input().split('-')
+print(a,b, sep = '')
