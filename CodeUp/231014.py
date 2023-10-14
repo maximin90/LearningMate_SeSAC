59f727ba35c87ae11b99e0b975ba6eeb9cbc61df
maxmin90 <91pp90@gmail.com>
2023-10-14 21:28:52 +0900

    6일차 코테 문제풀이 완료

---
+++ CodeUp/231014.py
@@ -0,0 +1,21 @@
+# 6026 
+f1 = float(input())
+f2 = float(input())
+print(f1+f2)
+
+# 6027
+n = int(input())
+print('%x'%n) #대문자 X로 쓰면 16진수 대문자로 출력
+
+# 6028
+n = int(input())
+print('%X' %n) #대문자 X로 쓰면 16진수 대문자로 출력
+
+# 6029
+a = input()
+n = int(a, 16)  # 한번 더 변환 필요 
+print('%o'%n)
+
+# 6030
+n = ord(input())
+print(n)
