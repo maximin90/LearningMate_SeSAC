419008979392754052b0f101e0f4aa4de0a76e95
maxmin90 <91pp90@gmail.com>
2023-10-14 21:19:22 +0900

    5일차 코테 문제풀이 완료

---
+++ CodeUp/231013.py
@@ -0,0 +1,21 @@
+# 6021
+s = input()
+for i in s:
+     print(i)
+
+# 6022
+birth = input()
+print(birth[0:2], birth[2:4], birth[4:6])
+
+# 6023
+time = input()
+t = time.split(':')
+print(t[1])
+
+# 6024
+w1, w2 = input().split()
+print
+
+# 6025
+a1, a2 = map(int, input().split())
+print(a1 + a2)
