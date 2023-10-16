70be5e09beb5cb54c78ef5036169fd29c89f662b
maxmin90 <91pp90@gmail.com>
2023-10-16 15:29:40 +0900

    7일차 코테 문제풀이 완료

---
+++ CodeUp/231015.py
@@ -0,0 +1,20 @@
+# 6031
+c = chr(int(input()))
+print(c)
+
+# 6032
+n = int(input())
+print(-(n))
+
+# 6033
+n = input()
+z = ord(n)+1
+print(chr(z+1))
+
+# 6034
+a, b = map(int, input().split())
+print(a - b)
+
+# 6035
+a, b = map(float, input().split())
+print(a * b)
