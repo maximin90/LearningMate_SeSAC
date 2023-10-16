cb970ef7593bd6f541239330a37f1e999481766a
maxmin90 <91pp90@gmail.com>
2023-10-16 15:37:28 +0900

    8일차 코테 문제풀이 완료

---
+++ CodeUp/231016.py
@@ -0,0 +1,20 @@
+# 6036
+a, b = input().split()
+print(a* int(b))
+
+# 6037
+a = int(input())
+b = input()
+print(a * b)
+
+# 6038
+a, b = map(int, input().split())
+print( a**b )
+
+# 6039
+a, b = map(float, input().split())
+print( a**b )
+
+# 6040
+a, b = map(int, input().split())
+print( a//b )
