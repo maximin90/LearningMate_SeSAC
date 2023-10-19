4d93be4c255df9cc82569893c712fa56f21c97b2
maxmin90 <91pp90@gmail.com>
2023-10-20 00:00:31 +0900

    10일차 코테 문제풀이 완료

---
+++ CodeUp/231018.py
@@ -0,0 +1,28 @@
+# 6046
+num = int(input())
+print(num << 1)
+
+# 6047
+num1, num2 = map(int, input().split())
+print(num1 << num2)
+
+# 6048
+num1, num2 = map(int, input().split())
+if num1 < num2:
+    print(True)
+else:
+    print(False) 
+
+# 6049
+num1, num2 = map(int, input().split())
+if num1 == num2:
+    print(True)
+else:
+    print(False) 
+
+# 6050
+num1, num2 = map(int, input().split())
+if num1 <= num2:
+    print(True)
+else:
+    print(False) 
